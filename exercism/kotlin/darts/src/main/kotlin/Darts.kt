40796d95e89253249040040438fc943697ef9020
Marcos de la Calle <marcos.dlcs@gmail.com>
2022-04-03 12:33:48 +0200

    Add new exercises

--- exercism/kotlin/darts/src/main/kotlin/Darts.kt
+++ exercism/kotlin/darts/src/main/kotlin/Darts.kt
@@ -1,6 +1,20 @@
+import kotlin.math.pow
+
 object Darts {
 
-    fun score(x: Any, y: Any /* choose proper types! */): Int {
-        TODO("Implement this function to complete the task")
+    fun score(x: Number, y: Number): Int {
+
+        val xD: Double = x.toDouble()
+        val yD: Double = y.toDouble()
+
+        if (isInside(1.0, xD, yD)) return 10
+        if (isInside(5.0, xD, yD)) return 5
+        if (isInside(10.0, xD, yD)) return 1
+
+        return 0
+    }
+
+    private fun isInside(rad: Double, x: Double, y: Double): Boolean {
+        return (x - 0.0).pow(2) + (y - 0.0).pow(2) <= rad.pow(2)
     }
 }
